f72f730d244ed0df361902e55a044ce5ecefbd1b
Marcos <marcosugaldemartinez@gmail.com>
2023-09-04 10:57:08 +0200

    feat: added query for selecting a list of playlists created by user

--- spoti-backend/models/spoti/queries.js
+++ spoti-backend/models/spoti/queries.js
@@ -8,6 +8,14 @@ const insertList = (listName, listURL, userId) => sql.unsafe`
     )
 `;
 
+const selectList = (userId) => sql.unsafe`
+        SELECT lists.list_name, lists.list_url, lists.created_by, users.username FROM lists 
+        INNER JOIN users
+        ON users.id = lists.created_by
+        WHERE lists.created_by = ${userId};
+`;
+
 module.exports = {
   insertList,
+  selectList,
 };
