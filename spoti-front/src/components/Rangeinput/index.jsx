681fa5107c388da0e47f8dd93673edfd47d359f8
Marcos <marcosugaldemartinez@gmail.com>
2023-09-05 15:54:18 +0200

    feat: created an independent component for range input

---
+++ spoti-front/src/components/Rangeinput/index.jsx
@@ -0,0 +1,9 @@
+const Inputrange = () => {
+    return (
+        <>
+            <input type="range" min="0" max="1" step="0.1" value="1"></input>
+        </>
+    )
+}
+
+export default Inputrange
