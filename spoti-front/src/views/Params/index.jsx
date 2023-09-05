07170d30c9c8d39e8ea961ea11943f42250adeb5
Marcos <marcosugaldemartinez@gmail.com>
2023-09-05 16:18:14 +0200

    style: added some styles to params page

--- spoti-front/src/views/Params/index.jsx
+++ spoti-front/src/views/Params/index.jsx
@@ -1,14 +1,15 @@
+import Styled from './styles'
 import Inputrange from "../../components/Rangeinput"
 
 const ParamsInsertion = () => {
     return (
-        <>
+        <Styled.Body>
             <h1>Parámetros manuales</h1>
             <div>
                 <Inputrange />
             </div>
             <button>Continuar</button>
-        </>
+        </Styled.Body>
     )
 }
 
---
+++ spoti-front/src/views/Params/styles.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+const Body = styled.section`
+    width: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center
+`
+
+export default {
+    Body,
+}
