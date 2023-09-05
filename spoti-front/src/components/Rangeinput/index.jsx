4ad662ab43b16736806eecbb627ace8ac41c4dc0
Marcos <marcosugaldemartinez@gmail.com>
2023-09-05 16:17:14 +0200

    style: added some styles to range input

--- spoti-front/src/components/Rangeinput/index.jsx
+++ spoti-front/src/components/Rangeinput/index.jsx
@@ -1,8 +1,16 @@
+import Styled from './styles'
+
 const Inputrange = () => {
     return (
-        <>
-            <input type="range" min="0" max="1" step="0.1" value="1"></input>
-        </>
+        
+            <Styled.Input>
+                <input type="range" id="instrumentalness" min="0" max="1" step="0.1" value="1"></input>
+                <Styled.Labels>
+                    <label htmlFor="instrumentalness" value="0">Instrumental</label>
+                    <label htmlFor="instrumentalness" value="1">Vocal</label>
+                </Styled.Labels>
+            </Styled.Input>
+        
     )
 }
 
---
+++ spoti-front/src/components/Rangeinput/styles.jsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+const Input = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center
+`
+
+const Labels = styled.section`
+    display: flex;
+`
+
+export default {
+    Input,
+    Labels,
+}
